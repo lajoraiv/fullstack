dfe42732ebbf138f7eef38f669a58d506329f12b
lajoraiv <lajoraiv@student.jyu.fi>
2024-06-25 14:11:53 +0300

    2.12-2.15 tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,25 +1,21 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-
-  useEffect(() => {
-    console.log('effect')
-      axios
-        .get('http://localhost:3001/persons')
-        .then(response => {
-           console.log('promise fulfilled')
-           setPersons(response.data)      
-          })
-        }, [])
-        console.log('render', persons.length, 'persons')
-
   
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
-
+  
+  
+  useEffect(() => {
+    personService
+    .getAll()
+      .then(initialData => {
+        setPersons(initialData)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     console.log('button clicked', event.target)
@@ -33,21 +29,37 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    for(var i = 0; i < persons.length; i++) {
-      if (persons[i].name == newName) {
-        alert(`${newName} is already added to phonebook`)
-        setNewName('')
-        setNewNumber('')
-        return
-      }
-    }
+    var matchFound = false
+
     const personObject = {
       name: newName,
       number: newNumber
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+
+    for(var i = 0; i < persons.length; i++) {
+      if (persons[i].name == newName) {
+        if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+          var id = persons[i].id
+          personService
+            .update(id, personObject).then(returnedPerson => {
+              setPersons(persons.map(person => person.id !== id ? person : returnedPerson ))
+            })
+        }
+        setNewName('')
+        setNewNumber('')
+        matchFound = true
+        return
+      }
+    }
+    if (matchFound === false) {
+        personService
+        .create(personObject)
+          .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
   }
 
   const [newFilter, setNewFilter] = useState('')
@@ -63,11 +75,23 @@ const App = () => {
 
   const showFiltered = (event) => {
     event.preventDefault()
+    console.log(newFilter)
     if (newFilter.length === 0) {
       setShowAll(true)
       return
     }
     setShowAll(false)
+  }
+
+  const removePerson = (id, name) => {
+    if (window.confirm(`Delete ${name} ?`)) {
+      personService
+        .remove(id)
+          .then(responseData => {
+            setPersons(persons.filter(n => n.id !== id))
+          })
+          .catch(error => { console.log("It didnt work", error)})
+        }
   }
 
   return (
@@ -77,52 +101,58 @@ const App = () => {
       <Header text="add a new"></Header>
       <AddNumber addPerson ={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <Header text="Numbers"></Header>
-      <Phonebook personsToShow={personsToShow} />
+      <Phonebook personsToShow={personsToShow} removePerson={removePerson}/>
     </div>
   )
 }
 
-const Header = (props) => {
-  return (
-    <div> 
-       <h2>{props.text}</h2>
+  const Header = (props) => {
+    return (
+      <div> 
+        <h2>{props.text}</h2>
+      </div>
+    )
+  }
+
+  const Filter = (props) => (
+    <form onSubmit={props.showFiltered}>
+          <div>
+            filter shown with: <input
+              value={props.newFilter}
+              onChange={props.handleFilterChange}/>
+          </div>
+        </form>
+  )
+
+  const AddNumber = (props) => (
+    <form onSubmit={props.addPerson}>
+          <div>
+            name: <input
+              value={props.newName}
+              onChange={props.handleNameChange}/>
+          </div>
+          <div>
+            number: <input
+              value={props.newNumber}
+              onChange={props.handleNumberChange}/>
+          </div>
+          <div>
+          <button type="submit">add</button>
+          </div>
+        </form>
+  )
+
+  const Phonebook = (props) => (
+    <div>
+      {props.personsToShow.map(person => 
+        <div key={person.id}>
+          {person.name} {person.number} 
+          <button onClick={() => props.removePerson(person.id, person.name)}>
+            delete
+          </button>
+        </div>
+      )}
     </div>
   )
-}
-
-const Filter = (props) => (
-  <form onChange={props.showFiltered}>
-        <div>
-          filter shown with: <input
-            value={props.newFilter}
-            onChange={props.handleFilterChange}/>
-        </div>
-      </form>
-)
-
-const AddNumber = (props) => (
-  <form onSubmit={props.addPerson}>
-        <div>
-          name: <input
-            value={props.newName}
-            onChange={props.handleNameChange}/>
-        </div>
-        <div>
-          number: <input
-            value={props.newNumber}
-            onChange={props.handleNumberChange}/>
-        </div>
-        <div>
-        <button type="submit">add</button>
-        </div>
-      </form>
-)
-const Phonebook = (props) => (
-  <div>
-    {props.personsToShow.map(person => 
-      <div key={person.name}>{person.name} {person.number}</div>
-    )}
-  </div>
-)
 
 export default App
